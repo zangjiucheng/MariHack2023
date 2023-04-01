0415d86e5babfc3c5a8ce60f367f029945dfd01f
zeze33629 <zeze33629@gmail.com>
2023-03-31 22:34:53 -0400

    Expansion in person

--- Food_Attribute.java
+++ Food_Attribute.java
@@ -1,6 +1,18 @@
-public class Food_Attribute{
+public class Food_Attribute {
     protected int calories, fat, sodium, sugar;
 
+    /* Constructor */
+    public Food_Attribute() {
+    }
+
+    public Food_Attribute(int calories, int fat, int sodium, int sugar) {
+        this.calories = calories;
+        this.fat = fat;
+        this.sodium = sodium;
+        this.sugar = sugar;
+    }
+
+    /* Getter setters */
     public int getCalories() {
         return this.calories;
     }
@@ -41,27 +53,18 @@ public class Food_Attribute{
             return false;
         }
         Food_Attribute food_Attribute = (Food_Attribute) o;
-        return calories == food_Attribute.calories && fat == food_Attribute.fat && sodium == food_Attribute.sodium && sugar == food_Attribute.sugar;
-    }
-
-    public Food_Attribute() {
-    }
-
-    public Food_Attribute(int calories, int fat, int sodium, int sugar) {
-        this.calories = calories;
-        this.fat = fat;
-        this.sodium = sodium;
-        this.sugar = sugar;
+        return calories == food_Attribute.calories && fat == food_Attribute.fat && sodium == food_Attribute.sodium
+                && sugar == food_Attribute.sugar;
     }
 
     @Override
     public String toString() {
         return "{" +
-            " calories='" + getCalories() + "'" +
-            ", fat='" + getFat() + "'" +
-            ", sodium='" + getSodium() + "'" +
-            ", sugar='" + getSugar() + "'" +
-            "}";
+                " calories='" + getCalories() + "'" +
+                ", fat='" + getFat() + "'" +
+                ", sodium='" + getSodium() + "'" +
+                ", sugar='" + getSugar() + "'" +
+                "}";
     }
 
 }
